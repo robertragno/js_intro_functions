8cb0b2e943c86976fad4333185d8afd5c74586b9
rragno <robertragno@gmail.com>
2014-10-01 14:38:05 -0700

    Fixed assignment

--- robert_ragno/app.js
+++ robert_ragno/app.js
@@ -2,15 +2,15 @@ var myAge = function(birth) {
     var time = new Date();
     var year = time.getFullYear();
     if (year - birth > 0) {
-    	return console.log("You are  either " + (year - birth) + " or " + (year - birth - 1) + " years old.");
+    	return "You are  either " + (year - birth) + " or " + (year - birth - 1) + " years old.";
     }
     else {
-    	return console.log("You haven't been conceived yet.");
+    	return "You haven't been conceived yet.";
     }
 };
 
-myAge(1986);
+console.log(myAge(1986));
 
-myAge(1990);
+console.log(myAge(1990));
 
-myAge(2016);
+console.log(myAge(2016));
